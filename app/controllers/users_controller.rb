a37431c171ec3f0de472d8e3ac3aea8ca8582894
rafael <rafi_tas@hotmail.com>
2014-06-27 19:26:10 -0500

    Proyecto de prueba

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,41 @@
+class UsersController < ApplicationController
+  #Con esto protegemos a las acciones dentoro de un controlador
+  #before_filter :require_login
+
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end 
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_url, :notice => "Se ha creado el usuario con exito"
+    else
+      render :new
+    end
+  end
+   
+  def destroy
+    logout
+    redirect_to root_url
+    #, :notice => "Logged out!"
+  end
+
+  def edit
+  end
+
+  def update
+  end  
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation, :nombre, :apellidos, :direccion, :telefono, :tipoUsuario)
+  end
+end
